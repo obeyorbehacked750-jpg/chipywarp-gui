7050efc47c717098a55e67a56ee99fe20113d800
obeyorbehacked750-jpg <obeyorbehacked750@gmail.com>
2026-08-26 10:10:21 +0300

    removed variable froom default project Yay

--- src/lib/default-project/project-data.js
+++ src/lib/default-project/project-data.js
@@ -26,12 +26,7 @@ const projectData = translateFunction => {
             {
                 isStage: true,
                 name: 'Stage',
-                variables: {
-                    '`jEk@4|i[#Fk?(8x)AV.-my variable': [
-                        translator(messages.variable),
-                        0
-                    ]
-                },
+                variables: {},
                 lists: {},
                 broadcasts: {},
                 blocks: {},
