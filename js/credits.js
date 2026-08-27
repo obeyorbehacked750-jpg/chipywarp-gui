bd3fdcfd0925ae89764f434398c0b79f71390b66
obeyorbehacked750-jpg <obeyorbehacked750-jpg@users.noreply.github.com>
2026-08-27 08:57:47 +0000

    deploy: eccd666576d8d4de5ebec86e74d560ec812c4b30

--- js/credits.js
+++ js/credits.js
@@ -322,24 +322,25 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "guiColors", function() { return guiColors; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "blockColors", function() { return blockColors; });
 const guiColors = {
-  'motion-primary': '#ff4c4c',
-  'motion-primary-transparent': '#ff4c4ce6',
-  'motion-tertiary': '#cc3333',
-  'looks-secondary': '#ff4c4c',
-  'looks-transparent': '#ff4d4d59',
-  'looks-light-transparent': '#ff4d4d26',
-  'looks-secondary-dark': 'hsla(0, 42%, 51%, 1)',
-  'extensions-primary': 'hsla(10, 85%, 65%, 1)',
-  'extensions-tertiary': 'hsla(10, 85%, 40%, 1)',
-  'extensions-transparent': 'hsla(10, 85%, 65%, 0.35)',
-  'extensions-light': 'hsla(10, 57%, 85%, 1)',
-  'drop-highlight': '#ff8c8c',
-  // eslint-disable-next-line max-len
-  'menu-bar-background-image': 'linear-gradient(90deg, rgba(255, 0, 0, 0.75) 0%, rgba(255, 154, 0, 0.75) 10%, rgba(208, 222, 33, 0.75) 20%, rgba(79, 220, 74, 0.75) 30%, rgba(63, 218, 216, 0.75) 40%, rgba(47, 201, 226, 0.75) 50%, rgba(28, 127, 238, 0.75) 60%, rgba(95, 21, 242, 0.75) 70%, rgba(186, 12, 248, 0.75) 80%, rgba(251, 7, 217, 0.75) 90%, rgba(255, 0, 0, 0.75) 100%)'
+  // Rainbow effect (RGB)
+  'motion-primary': 'hsl(var(--rgb-hue, 0deg), 100%, 60%)',
+  'motion-primary-transparent': 'hsla(var(--rgb-hue, 0deg), 100%, 60%, 0.9)',
+  'motion-tertiary': 'hsl(var(--rgb-hue, 0deg), 100%, 40%)',
+  'looks-secondary': 'hsl(var(--rgb-hue, 0deg), 100%, 60%)',
+  'looks-transparent': 'hsla(var(--rgb-hue, 0deg), 100%, 60%, 0.35)',
+  'looks-light-transparent': 'hsla(var(--rgb-hue, 0deg), 100%, 60%, 0.15)',
+  'looks-secondary-dark': 'hsl(var(--rgb-hue, 0deg), 100%, 45%)',
+  'extensions-primary': 'hsl(var(--rgb-hue, 0deg), 85%, 65%)',
+  'extensions-tertiary': 'hsl(var(--rgb-hue, 0deg), 85%, 40%)',
+  'extensions-transparent': 'hsla(var(--rgb-hue, 0deg), 85%, 65%, 0.35)',
+  'extensions-light': 'hsl(var(--rgb-hue, 0deg), 57%, 85%)',
+  'drop-highlight': 'hsl(var(--rgb-hue, 0deg), 100%, 75%)',
+  'menu-bar-background-default': 'hsl(var(--rgb-hue, 0deg), 85%, 45%)',
+  'menu-bar-background-image': 'none'
 };
 const blockColors = {
-  checkboxActiveBackground: '#ff4c4c',
-  checkboxActiveBorder: '#cc3333'
+  checkboxActiveBackground: 'hsl(var(--rgb-hue, 0deg), 100%, 60%)',
+  checkboxActiveBorder: 'hsl(var(--rgb-hue, 0deg), 100%, 40%)'
 };
 
 
